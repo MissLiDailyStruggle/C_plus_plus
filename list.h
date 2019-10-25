76175cbc1e41f217824858672d8d6f662f9abe43
MissLiDilayStruggle <18792313295@163.com>
2019-10-25 15:18:42 +0800

    list类的简单实现 第二次提交

--- list.h
+++ list.h
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
 namespace li
 {
 	template<class T>
@@ -6,51 +10,76 @@ namespace li
 		_list_node<T>* _next;
 		_list_node<T>* _prev;
 		T data;
-		_list_node(const T x)
+		_list_node(const T x = T())
 			:data(x),
 		    _next(nullptr),
 		    _prev(nullptr)
 		{}
 	};
-	template<class T>
+	template<class T,class Ref, class Ptr>
 	struct _list_iterator
 	{
 		typedef _list_node<T> node;
+		typedef _list_iterator<T, Ref, Ptr> Self;
 		node *_node;
 		_list_iterator(node *node)
 			:_node(node)
 		{}
-		T& operator*()
+		Ref operator*()//内置类型
 		{
 			return _node->data;
 		}
-		_list_iterator<T>& operator++()
+		Ptr operator->()//自定义类型
+		{
+			return &_node->data;
+		}
+		
+		Self& operator++()
 		{
 			_node = _node->_next;
 			return *this;
 		}
-		_list_iterator<T> operator++(int)
+		Self operator++(int)
 		{
 			_list_iterator<T> tmp(*this);
 			_node = _node->_next;
 			return tmp;
 		}
-		bool operator!=(_list_iterator<T>& it)
+		Self& operator--()
+		{
+			_node = _node->_prev;
+			return *this;
+		}
+		Self operator--(int)
+		{
+			_list_iterator<T> tmp(*this);
+			_node = _node->_prev;
+			return tmp;
+		}
+		bool operator!=(const Self& it)
 		{
 			return _node != it._node;
+		}
+		bool operator==(const Self& it)
+		{
+			return _node == it._node;
 		}
 	};
 	template<class T>
 	class list
 	{
-	public:
 		typedef _list_node<T> node;
-		typedef  _list_iterator<T> iterator;
-		list()
+	public:
+		typedef  _list_iterator<T,T&,T*> iterator;
+		typedef  _list_iterator<T,const T&,const T*> const_iterator;
+
+		const_iterator begin()  const
 		{
-			_head = new node(T());
-			_head->_next = _head;
-			_head->_prev = _head;
+			return const_iterator(_head->_next);
+		}
+		const_iterator end()  const
+		{
+			return const_iterator(_head);
 		}
 		iterator begin()
 		{
@@ -60,16 +89,105 @@ namespace li
 		{
 			return iterator(_head);
 		}
+
+		list()
+		{
+			_head = new node();
+			_head->_next = _head;
+			_head->_prev = _head;
+		}
+		~list()
+		{
+			clear();
+			delete _head;
+			_head = nullptr;
+		}
+		list(const list<T>& l)
+		{
+			_head = new node;
+			_head->_next = _head;
+			_head->_prev = _head;
+			const_iterator it = l.begin();
+			while (it != l.end())
+			{
+				push_back(*it);
+				++it;
+			}
+		}
+		list<T>& operator=(const list<T> l)
+		{
+			swap(_head, l._head);
+			return *this;
+		}
+		void clear()
+		{
+			iterator it = begin();
+			while (it != end())
+			{
+				it = erase(it);
+			}
+		}
+
+		void insert(iterator pos,const T& x)
+		{
+			node *cur = pos._node;
+			node *prev = cur->_prev;
+			node *newnode = new node(x);
+			
+			prev->_next = newnode;
+			newnode->_prev = prev;
+			newnode->_next = cur;
+			cur->_prev = newnode;
+		}
+		iterator erase(iterator pos)
+		{
+			node *cur = pos._node;
+			assert(cur != _head); 
+			node *prev = cur->_prev;
+			node *next = cur->_next;
+
+			prev->_next = next;
+			next->_prev = prev;
+			delete cur;
+
+			return iterator(next);
+		}
 		void push_back(const T& x)
 		{
-			node *tail = _head->_prev;
+			/*node *tail = _head->_prev;
 			node *newnode = new node(x);
 			tail->_next = newnode;
 			newnode->_prev = tail;
 			newnode->_next = _head;
-			_head->_prev = newnode;
+			_head->_prev = newnode;*/
+			insert(end(), x);
+		}
+		void push_front(const T& x)
+		{
+			
+			insert(begin(), x);
+		}
+		void pop_back()
+		{
+
+			erase(--end());
+		}
+		void pop_front()
+		{
+
+			erase(begin());
 		}
 	private:
 		node *_head;
 	};
+	void print_list(const list<int> & l)
+	{
+		list<int>::const_iterator it = l.begin();
+		while (it != l.end())
+		{
+			cout << *it << " ";
+			++it;
+		}
+		cout << endl;
+	}
 };
--- list_test.cpp
+++ list_test.cpp
@@ -1,9 +1,7 @@
-#include <iostream>
 #include "list.h"
 
-using namespace std;
 
-void list_list()
+void list_text1()
 {
 	li::list<int> ex;
 	ex.push_back(1);
@@ -11,17 +9,46 @@ void list_list()
 	ex.push_back(6);
 	ex.push_back(7);
 	ex.push_back(0);
+	li::list<int>ex2(ex);
+	li::list<int>ex3 = ex;
+	li::print_list(ex);
+	li::print_list(ex2);
+	li::print_list(ex3);
 	li::list<int>::iterator it = ex.begin();
 	while (it != ex.end())
 	{
-		cout << *it << endl;
+		if (*it % 2 == 0)
+		{
+			it = ex.erase(it);
+		}
+		++it;
+	}
+	li::print_list(ex);
+}
+
+struct Date
+{
+	int _year = 1990;
+	int _month = 1;
+	int _day = 1;
+};
+void list_text2()
+{
+	li::list<Date> l;
+	l.push_back(Date());
+	l.push_back(Date());
+	auto it = l.begin();
+	while (it != l.end())
+	{
+		cout << it->_year << "-" << it->_month << "-" << it->_day << endl;
 		++it;
 	}
 }
 
 int main()
 {
-	list_list();
+	//list_text1();
+	list_text2();
 	system("pause");
 	return 0;
 }
